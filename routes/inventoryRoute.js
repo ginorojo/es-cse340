4072e4eeba986661f933efc635a2d9d587e24d83
ginorojo <ginorojoj@gmail.com>
2025-09-28 23:44:10 -0300

    learning activities

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,11 +3,12 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities");
-
+const accountController = require("../controllers/accountController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 router.get("/detail/:invId", utilities.handleErrors(invController.buildByInventoryId));
 router.get("/test/error500", (req, res, next) => {
     next(new Error("this is an intentional 500 error !"))
 })
+
 module.exports = router;
--- server.js
+++ server.js
@@ -13,11 +13,40 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const utilities = require("./utilities/index")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 
 /* ***********************
  *View Engine and Templates
- *************************/
+ ***************** ********/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -30,7 +59,8 @@ app.use(require("./routes/static"));
 app.get("/", utilities.handleErrors(baseController.buildHome))
 //inventory routes
 app.use("/inv", require("./routes/inventoryRoute"))
-
+//acount routes
+app.use("/account",require("./routes/accountRoute"))
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
